37b150154af93e3f389d4bd3f4df42895ab7fa20
commitDonMarcos <dondon.marcos.github@gmail.com>
2026-08-26 14:55:00 +0800

    fix: add favicon link to the application header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="csrf-token" content="{{ csrf_token() }}" />
   <title>NEUST Faculty Evaluation System</title>
+  <link rel="icon" type="image/webp" href="{{ asset('assets/img/neust_logo.webp') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
   @vite(['resources/js/app.js'])
 </head>
